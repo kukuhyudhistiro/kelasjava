f3068c4c160fa32fa102c501342b42b160ef0ac9
kukuhyudhistiro <kukuh.yudhistiro@gmail.com>
2019-10-16 15:01:26 +0700

    Lanjutan transaski menggunakan array 2 dim
    Lanjutan transaski menggunakan array 2 dim

--- array2dimen.java
+++ array2dimen.java
@@ -12,22 +12,30 @@ import java.util.Scanner;
 
 public class array2dimen {
      public static void main(String args[]) {
-          byte tujuan; //1=SBY, 2=BDG
-          String kotapilihan = "";
-          int hrgpilihan = 0;
-          int jrkpilihan =  0;
-          
-         String kota[][]={{"1","SURABAYA","169","2500"},{"2","BANDUNG","452","4000"}};
-         
-           System.out.println("--------------------------------------------------");
-            System.out.println("-          Perusahaan             -");
-            System.out.println("-    Ekspedisi Lorena Malang      -");
-            System.out.println("--------------------------------------------------");
-            System.out.println("Kode    | TUJUAN     |  jarak/km  | ONGKIR/km  ");
-            System.out.println("--------------------------------------------------");
-         for(int baris = 0; baris<2;baris++){
+        byte tujuan=0; //1=SBY, 2=BDG
+        String kotapilihan = "";
+        int hrgpilihan = 0;
+        int jrkpilihan =  0;
+        int totbayar =0;          
+
+        String kota[][]={   
+                            {"1","SURABAYA","169","2500"},
+                            {"2","BANDUNG","452","4000"},
+                            {"3","SEMARANG","300","4500"},
+                            {"4","BOGOR","350","5000"},
+                            {"5","SALATIGA","250","5500"},
+                            {"6","DENPASAR","500","6000"},
+                        };
+
+        System.out.println("--------------------------------------------------");
+        System.out.println("-          Perusahaan             -");
+        System.out.println("-    Ekspedisi Lorena Malang      -");
+        System.out.println("--------------------------------------------------");
+        System.out.println("Kode    | TUJUAN     |  jarak/km  | ONGKIR/km  ");
+        System.out.println("--------------------------------------------------");
+         for(int baris = 0; baris<6;baris++){
              for(int kolom = 0; kolom <4; kolom++ ){
-                 System.out.print(kota[baris][kolom] + "    | ");
+                 System.out.print(kota[baris][kolom] + "  - ");
              }
              System.out.println("");
          }
@@ -37,6 +45,28 @@ public class array2dimen {
         System.out.print("-> Pilih kode tujuan  = ");
         tujuan = inputtujuan.nextByte();
         
-        //lanjutkan hingga proses cetak / tampil nota
+        //ambil data array
+        //kota[baris][kolom]
+        kotapilihan = kota[tujuan-1][1];
+        jrkpilihan = Integer.parseInt(kota[tujuan-1][2]);
+        hrgpilihan = Integer.parseInt(kota[tujuan-1][3]);
+        
+        totbayar = hrgpilihan * jrkpilihan;
+        //report
+        System.out.println("-------------------------------------");
+        System.out.println("Anda Memilih       : " + kotapilihan);
+        System.out.println("Biaya Pengiriman   : " + hrgpilihan + " km * " + jrkpilihan);
+        System.out.println("                   : " + totbayar);
+        System.out.println("-------------------------------------");
+        
+        //lenkapi dengan looping 
+        //layar harus di ClEAR
+        //pemberian diskon
+        //soal:
+        //1. tambahkan Inputan untuk Berat (tarif per kg Rp6000)
+        //2. total bayar berdasarkan jarak + biaya berdasarkan berat/kg
+        //3. jika subtotal bayar > 750000, diskon 5%
+        //4. tampilkan total.
+        
      }
 }
